cc79b3a3ef8a6b07d9facefcc921240846f00170
Lance <gonzalesraymundlance0729@gmail.com>
2024-07-16 18:00:03 +0800

    Finished exercise 05 in javascript

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,25 @@
-const sumAll = function() {
-
+const sumAll = function(firstNum, secondNum) {
+    const message = "ERROR";
+    let sum = 0;
+    if(!Number.isInteger(firstNum) || !Number.isInteger(secondNum)) {
+        return message;
+    }
+    else if(firstNum < 0 || secondNum < 0) {
+        return message;
+    }
+    else if(firstNum < secondNum) {
+        for(let i = firstNum; i < secondNum; i++) {
+            sum += i;
+        }
+        return sum + secondNum;
+    }
+    else if(firstNum > secondNum) {
+        for(let i = secondNum; i < firstNum; i++) {
+            sum += i;
+        }
+        return sum + firstNum;
+    }
+    
 };
 
 // Do not edit below this line
