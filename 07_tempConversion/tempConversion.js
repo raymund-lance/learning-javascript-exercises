42480e374a9a4de5f319be9576a004578bd816c1
Lance <gonzalesraymundlance0729@gmail.com>
2024-07-16 18:36:09 +0800

    Finished exercise 07 rounding decimal javascript

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,15 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(cel) {
+    let convert = 0;
+    convert = (cel - 32) * (5/9);
+    convert = Math.round(convert * 10) / 10;
+    return convert
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(fahr) {
+  let convert = 0;
+  convert = (fahr * 9/5) + 32;
+  convert = Math.round(convert * 10) / 10;
+  return convert;
 };
 
 // Do not edit below this line
