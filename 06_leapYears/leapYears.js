0739fc2a58a9cff9429fc2e49173a2593bfbb344
Lance <gonzalesraymundlance0729@gmail.com>
2024-07-16 18:15:59 +0800

    Finished exercise 06 leapyear in javascript

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,10 @@
-const leapYears = function() {
-
+const leapYears = function(leap) {
+    if (leap % 4 == 0 && (!(leap % 100 == 0) || leap % 400 == 0)) {
+        return true;
+    }
+    else {
+        return false;
+    }
 };
 
 // Do not edit below this line
